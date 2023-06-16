d506a2409faa148110ae9ec483cdfa6c1662f1d4
Zineb112 <zbelassiri@gmail.con>
2023-06-16 15:40:15 -0700

    header file shell.h added

---
+++ shell.h
@@ -0,0 +1,19 @@
+#ifndef _shell_h
+#define _shell_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+
+#endif
