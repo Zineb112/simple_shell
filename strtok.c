039cc50c475435e910c0a6853b0e974a338d701f
kawakibb <kawakibk7@gmail.com>
2023-06-21 15:25:46 -0700

    strtok No_desription

--- strtok.c
+++ strtok.c
@@ -1,27 +1,34 @@
 #include "shell.h"
 
-char* _strtok(char* str, const char* delimiters) {
-	static char* token1 = NULL;
-	static char* tokens = NULL;   
+char *_strtok(char *str, const char *delimiters)
+{
+	static char *token1;
+	static char *tokens;
 
-	if (str != NULL) {
+	if (str != NULL)
+{
 		token1 = str;
-	} else if (tokens == NULL) {
-		return NULL;
-	} else {
+	} else if (tokens == NULL)
+{
+		return (NULL);
+	} else
+{
 		token1 = tokens;
 	}
 
-	char* delim_pos = token1;
-	while (*delim_pos != '\0') {
-		if (*delim_pos == *delimiters) {
+	char *delim_pos = token1;
+
+	while (*delim_pos != '\0')
+{
+		if (*delim_pos == *delimiters)
+{
 			*delim_pos = '\0';
 			tokens = delimi_pos + 1;
-			return token1;
+			return (token1);
 		}
 		delim_pos++;
 	}
 
-	tokens = NULL;  
-	return token1;
+	tokens = NULL;
+	return (token1);
 }
