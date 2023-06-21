ba34896262a613af9a7f4f6eda55b2c8f95822f7
kawakibb <kawakibk7@gmail.com>
2023-06-21 13:10:36 -0700

    betty No_description

--- getline.c
+++ getline.c
@@ -1,42 +1,45 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-ssize_t _getline(char **line, size_t *n, FILE *stream) {
-    size_t bf_size = 32;
-    ssize_t nbr_char = 0;
-    int c;
+ssize_t _getline(char **line, size_t *n, FILE *stream)
+{
+	size_t bf_size = 32;
+	ssize_t nbr_char = 0;
+	int c;
 
-    if (*line == NULL) {
-        *line = malloc(bf_size * sizeof(char));
-        if (*line == NULL) {
-            perror("malloc failed");
-            exit(EXIT_FAILURE);
-        }
-    }
+	if (*line == NULL)
+{
+		*line = malloc(bf_size * sizeof(char));
+		if (*line == NULL)
+{
+			perror("malloc failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	while ((c = fgetc(stream)) != EOF)
+{
+		if (nbr_char >= bf_size - 1)
+{
+			bf_size *= 2;
+			char *temp = realloc(*line, bf_size * sizeof(char));
 
-    while ((c = fgetc(stream)) != EOF) {
-        if (nbr_char >= bf_size - 1) {
-            bf_size *= 2;
-            char *temp = realloc(*line, bf_size * sizeof(char));
-            if (temp == NULL) {
-                perror("realloc failed");
-                exit(EXIT_FAILURE);
-            }
-            *line = temp;
-        }
+			if (temp == NULL)
+			{	perror("realloc failed");
+				exit(EXIT_FAILURE);
+			}
+			*line = temp;
+		}
+		(*line)[nbr_char++] = (char)c;
 
-        (*line)[nbr_char++] = (char)c;
+		if (c == '\n')
+			break;
+	}
 
-        if (c == '\n')
-            break;
-    }
+	if (nbr_char == 0)
+		return (-1);
 
-    if (nbr_char == 0)
-        return -1;
+	(*line)[nbr_char] = '\0';
+	*n = nbr_char;
 
-    (*line)[nbr_char] = '\0';
-    *n = nbr_char;
-
-    return nbr_char;
+	return (nbr_char);
 }
+
