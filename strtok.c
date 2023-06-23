73e2ccc354635af5973ee37d5f82fe62794405a1
Zineb112 <zbelassiri@gmail.con>
2023-06-22 17:18:27 -0700

    _strtok betty fixed

--- strtok.c
+++ strtok.c
@@ -1,48 +1,59 @@
 #include "shell.h"
 
+/**
+ * _strtok - split string.
+ * @str: string to split.
+ * @delimiters: separator.
+ * Return: splited strings.
+ */
 char *_strtok(char *str, const char *delimiters)
 {
-		static char *token1;
-		static char *tokens;
+	static char *token1;
+	static char *tokens;
 
-		if (str != NULL)
-{
- 	 	 	 	token1 = str;
-		} else if (tokens == NULL)
-{
-				return (NULL);
-		} else
-{
-				token1 = tokens;
+	if (str != NULL)
+	{
+		token1 = str;
+	}
+	else if (tokens == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		token1 = tokens;
+	}
+
+	char *delim_pos = token1;
+
+	while (*delim_pos != '\0')
+	{
+		if (*delim_pos == *delimiters)
+		{
+			*delim_pos = '\0';
+			tokens = delim_pos + 1;
+			return (token1);
 		}
+		delim_pos++;
+	}
+	tokens = NULL;
 
-		char *delim_pos = token1;
+	return (token1);
+}
 
-		while (*delim_pos != '\0')
-{
-				if (*delim_pos == *delimiters)
-{
-						*delim_pos = '\0';
- 	 					tokens = delim_pos + 1;
-						return (token1);
-				}
-				delim_pos++;
-		}
 
-		tokens = NULL;
-		return (token1);
-} 	 	 	 	 	 	 	 	 	 	 	 	 
 
 
 int main() {
-	char str[] = "Hello World!";
-	const char delimiters[] = " ";
+        char str[] = "Hello World of C language";
+        const char delimiters[] = " ";
 
-	char *token = _strtok(str, delimiters);
-	while (token != NULL) {
-		printf("%s\n", token);
-		token = _strtok(NULL, delimiters);
-	}
+        char *token = _strtok(str, delimiters);
+        while (token != NULL) {
+                printf("%s\n", token);
+                token = _strtok(NULL, delimiters);
+        }
 
-	return (0);
+        return (0);
 }
+
