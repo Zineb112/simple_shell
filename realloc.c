ca51ac5fcfbd9b32da063dc4b0ec358e6a0918ec
kawakibb <kawakibk7@gmail.com>
2023-06-26 09:40:16 -0700

    realloc

---
+++ realloc.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+#include <string.h>
+
+/**
+ *_realloc – reallocate size of a pointer
+ *@ptr: pointer to change size
+ *@size: old size of ptr
+ *Return: new pointer
+ */
+
+
+void *_realloc(void *ptr, size_t size)
+{
+	void *new_ptr;
+	size_t old_size, copy_size;
+
+	old_size = get_size(ptr);
+	copy_size = (old_size < size) ? old_size : size;
+	new_ptr = malloc(size);
+	if (new_ptr == NULL)
+		return (NULL);
+	new_ptr = memcpy(new_ptr, ptr, copy_size);
+	free(ptr);
+	return (new_ptr);
+}
+
