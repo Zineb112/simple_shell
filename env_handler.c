eb38522ccb8dda9645f20d464d18d1c96e1fb86b
kawakibb <kawakibk7@gmail.com>
2023-06-27 16:33:05 -0700

    fix error

--- env_handler.c
+++ env_handler.c
@@ -1,24 +1,3 @@
 #include "shell.h"
 
-/**
- * _getenv - get variables from the enviroment.
- * @env_name: variable's name.
- * Return: NULL.
- */
-char *_getenv(char *env_name)
-{
-	int x;
-	char *key;
 
-	x = 0;
-	while (environ[x])
-	{
-		key = strtok(environ[x], "=");
-		if (_strcmp(env_name, key) == 0)
-		{
-			return (strtok(NULL, "\n"));
-		}
-		x++;
-	}
-	return (NULL);
-}
--- shell.h
+++ shell.h
@@ -17,44 +17,5 @@
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
 
-/* Strings handling function */
-unsigned int _strlen(const char *str);
-void _strcat(char *str_1, char *str_2);
-int _strcmp(char *str_1, char *str_2);
-void _strcpy(char *desr, char *src);
-int _strncmp(char *str_1, char *str_2, size_t n);
-
-
-/* printing functions */
-void _puts(const char *str);
-int _putchar(char c);
-void str_print(char *str);
-
-
-/* split string functions */
-char *str_tok(char str[], char *delim);
-unsigned int is_delim(char c, char *delim);
-char *_strtok(char *srcString, char *delim);
-char **split_string(char *str, char *delimiters);
-
-/* our getline function */
-ssize_t _getline(char **line, size_t *n, FILE *stream);
-
-/* environment variables handling */
-int _setenv(char *env_name, char *env_val, int status, char ***environ_ptr);
-
-
-/* Golbal variable */
-extern char **environ;
-
-char *_getenv(char *name);
-char *getcmd(char *cmd);
-char **split_string(char *buffer, char *del);
-int main(int ac, char **av, char **env);
-
-/*realloc */
-void *_realloc(void *ptr, size_t size);
-void *_memcpy(void *destination, const void *src, size_t size);
-int _getsize(const char *str);
 
 #endif
